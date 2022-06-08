bbcadd969bfe4ae5e461869a9817da1b40b5018b
zhLisha <lisazhou1709@gmail.com>
2022-06-08 13:21:53 +0200

    first commit

---
+++ gioco-dadi/js/script.js
@@ -0,0 +1,5 @@
+/* 
+- Generare numero random da 1 a 6 per l'utente e computer
+- Stabilire il vincitore in base al numero maggiore
+- Messaggio all'utente dell'esito della partita
+*/
---
+++ mail/js/script.js
@@ -0,0 +1,7 @@
+/* 
+- Chiedere all'utente la sua email
+- Controllare che sia nella lista di chi puo' accedere
+- Stampare messaggio di esito positivo o negativo
+*/ 
+
+
